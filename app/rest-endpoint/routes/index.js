575c63263eeeb0a12dc167a9c67d2ca70cd1c8bf
Max Randolph <azzmodious@gmail.com>
2017-01-16 11:51:17 -0500

    using the rpi-gpio lib now

--- app/rest-endpoint/routes/index.js
+++ app/rest-endpoint/routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var gpio = require("pi-gpio");
-
-router.get('/', function(req, res, next){
+var gpio2 = require('rpi-gpio');
+router.get('/', function (req, res, next) {
     res.send('Hello World');
 });
 
@@ -10,40 +10,63 @@ router.get('/status', sendStatus);
 router.get('/device/:flag', changeDeviceState);
 
 
-function sendStatus(req, res){
+function sendStatus(req, res) {
     res.json({
         deviceName: 'heaterpi',
-        flame: 'off', 
+        flame: 'off',
         pumps: 'off'
     });
 }
 
-function changeDeviceState(req, res){
-    if(req.params.flag === 'on'){
+function changeDeviceState(req, res) {
+    if (req.params.flag === 'on') {
         res.send('device: on');
-        turnOnDevice();
+        //turnOnDevice();
+        altOnDevice();
     }
-    if(req.params.flag === 'off'){
+    if (req.params.flag === 'off') {
         res.send('device: off');
+        altOffDevice();
     }
 
 }
 
-function turnOnDevice(){
+function altOnDevice() {
+    gpio.setup(11, gpio.DIR_OUT, write);
 
-	gpio.open(11, "output", function(err){
-		gpio.write(11, 1, function(){
-		//gpio.close(11);
-		console.info("Relay on");
-		});	
-	});
+    function write() {
+        gpio.write(11, true, function (err) {
+            if (err) throw err;
+            console.log('Written to pin');
+        });
+    }
 }
 
-function cleanup(options, err){
+function altOffDevice() {
+    gpio.setup(11, gpio.DIR_OUT, write);
+
+    function write() {
+        gpio.write(11, false, function (err) {
+            if (err) throw err;
+            console.log('Written to pin');
+        });
+    }
+}
+function turnOnDevice() {
+
+    gpio.open(11, "output", function (err) {
+        gpio.write(11, 1, function () {
+            //gpio.close(11);
+            console.info("Relay on");
+        });
+    });
+}
+
+function cleanup(options, err) {
     console.info('cleaning things up...');
     gpio.close(11);
 }
 
-process.on('SIGINT', cleanup.bind(null, {exit:true}));
+process.on('SIGINT', cleanup.bind(null, { exit: true }));
 module.exports = router;
 //module.exports = gpio;
